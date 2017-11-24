48c72351e6310dbc0ef057a82a9f7071227dec99
Carolina Fernandez <carolina.fernandez@i2cat.net>
2017-11-24 11:46:20 +0100

    Divide further

---
+++ filter_plugins/dict.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import re
+
+
+class FilterModule(object):
+    def filters(self):
+        return {
+            "get_dict_key": self.get_dict_key,
+            "get_dict_value": self.get_dict_value,
+            "get_dict_value_for_key": self.get_dict_value_for_key,
+        }
+
+    def match_get_nth_group(self, reg_exp, data, nth):
+        reg_match = re.match(reg_exp, str(data))
+        if reg_match is not None and reg_match.groups >= (nth + 1):
+            return reg_match.group(nth)
+        return data
+
+    def get_dict_key(self, data):
+        reg_exp = "{(?:u')?(.*?)(?:')?: (?:u')?(.*?)(?:')?}"
+        return self.match_get_nth_group(reg_exp, data, 1)
+
+    def get_dict_value(self, data):
+        reg_exp = "{(?:u')?(?:.*?)(?:')?: (?:u')?(.*?)(?:')?}"
+        return self.match_get_nth_group(reg_exp, data, 1)
+
+    def get_dict_value_for_key(self, data, key):
+        reg_exp = "{(?:u')?%s(?:')?: (?:u')?(.*?)(?:')?}".replace("%s", key)
+        if isinstance(data, list):
+            data = data[0]
+        return self.match_get_nth_group(reg_exp, data, 1)
--- filter_plugins/net.py
+++ filter_plugins/net.py
@@ -6,9 +6,6 @@ import re
 class FilterModule(object):
     def filters(self):
         return {
-            "get_dict_key": self.get_dict_key,
-            "get_dict_value": self.get_dict_value,
-            "get_dict_value_for_key": self.get_dict_value_for_key,
             "iface_exists": self.iface_exists,
             "iface_in_target_node": self.iface_in_target_node,
             "ip_compute_subnet": self.ip_compute_subnet,
@@ -20,20 +17,6 @@ class FilterModule(object):
         if reg_match is not None and reg_match.groups >= (nth + 1):
             return reg_match.group(nth)
         return data
-
-    def get_dict_key(self, data):
-        reg_exp = "{(?:u')?(.*?)(?:')?: (?:u')?(.*?)(?:')?}"
-        return self.match_get_nth_group(reg_exp, data, 1)
-
-    def get_dict_value(self, data):
-        reg_exp = "{(?:u')?(?:.*?)(?:')?: (?:u')?(.*?)(?:')?}"
-        return self.match_get_nth_group(reg_exp, data, 1)
-
-    def get_dict_value_for_key(self, data, key):
-        reg_exp = "{(?:u')?%s(?:')?: (?:u')?(.*?)(?:')?}".replace("%s", key)
-        if isinstance(data, list):
-            data = data[0]
-        return self.match_get_nth_group(reg_exp, data, 1)
 
     def iface_exists(self, data, hostvars):
         return data in hostvars["ansible_interfaces"]
